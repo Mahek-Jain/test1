428a4a12ce6671b77af9b1f3d7c2bebc90392e91
Mahek Jain <mahek@Maheks-MacBook-Air.local>
2024-11-20 12:54:21 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TreeMenu from "./component/TreeMenu.js";
+import Modal from "./component/modal.jsx";
 
-function App() {
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false); //checking if modal box is open or not
+  const [modalContent, setModalContent] = useState(""); //storing the modal content
+
+  const handleNodeClick = (nodeName) => {
+    setModalContent(nodeName); //setting content for modal box
+    setIsModalOpen(true); //then opening the box
+  };
+  //tree menu data
+  const menuData = [
+    {
+      id: "root",
+      name: "Root",
+      children: [
+        {
+          id: "parentA",
+          name: "Parent A",
+          children: [
+            { id: "childA1", name: "Child A1" },
+            {
+              id: "childParentA2",
+              name: "Child Parent A2",
+              children: [
+                { id: "childA21", name: "Child A21" },
+                { id: "childA22", name: "Child A22" },
+              ],
+            },
+          ],
+        },
+        {
+          id: "parentB",
+          name: "Parent B",
+          children: [
+            { id: "childB1", name: "Child B1" },
+            { id: "childB2", name: "Child B2" },
+            {
+              id: "childParentB3",
+              name: "Child Parent B3",
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Dynamic Tree Menu</h1>
+      <TreeMenu data={menuData} handleNodeClick={handleNodeClick} />
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <h2>{modalContent}</h2>
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
